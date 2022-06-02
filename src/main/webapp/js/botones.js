b2e9a634ed078d7eabfbe2dc1138deb4cceaef7c
RenattoGuzman <78181499+RenattoGuzman@users.noreply.github.com>
2022-06-02 09:51:05 -0600

    LogIn boton pt1

--- src/main/webapp/js/botones.js
+++ src/main/webapp/js/botones.js
@@ -1,14 +1,28 @@
 (function($) {
   "use strict"; // Start of use strict
   
-  	//Evento del botón que creara un
+  	//Evento del botón que creara un usuario
 	$("#btnCreateProfile").click(function(){
 				
 		$.ajax( {
 			
 			type: "GET",
-																																																																			
+
 		url: '/Ohtell/SaveUserServlet?name=' + $('#nombres').val() + '&lastname=' + $('#con_lname').val() + '&password=' + $('#cb_password').val() + '&user=' + $('#cb_user').val() + '&comment=' + $('#con_message').val() + '&breakfast=' + $('#checkbox4').val() + '&calification=' + $('#cb_calification').val() + '&pets=' + $('#checkbox1').val() + '&pool=' + $('#checkbox3').val() + '&price=' + $('#ex4').val() + '&wifi=' + $('#checkbox2').val() + '&typeplace=' + $('#cb_typeplace').val(),
+			success: function(data) {
+			    alert("Resultado: " + data.resultado);
+			}
+		} );
+	});
+
+  	//Evento del botón para iniciar sesión
+	$("#btn_iniciarsesion").click(function(){
+				
+		$.ajax( {
+			
+			type: "GET",
+
+		url: '/Ohtell/UserLogInServlet?user=' + $('#cb_user_si').val() + '&password=' + $('#cb_password_si').val(),
 			success: function(data) {
 			    alert("Resultado: " + data.resultado);
 			}
