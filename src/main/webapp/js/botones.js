baadb55e0969939b7caf28de5167f7a500d3d860
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-06-02 19:00:05 -0600

    Iniciar sesion finalizada 100/100

--- src/main/webapp/js/botones.js
+++ src/main/webapp/js/botones.js
@@ -11,12 +11,12 @@
 
 			url: '/Ohtell1/SaveUserServlet?name=' + $('#nombres').val() + '&lastname=' + $('#con_lname').val() + '&password=' + $('#cb_password').val() + '&user=' + $('#cb_user').val() + '&comment=' + $('#con_message').val() + '&breakfast=' + $('#checkbox4').val() + '&calification=' + $('#cb_calification').val() + '&pets=' + $('#checkbox1').val() + '&pool=' + $('#checkbox3').val() + '&price=' + $('#ex4').val() + '&wifi=' + $('#checkbox2').val() + '&typeplace=' + $('#cb_typeplace').val(),
 			success: function(data) {
-				alert("Resultado: " + data.resultado);
-				
-				
+				location.href = "portfolio-3-col.html";
+				alert("Resultado: " + data.resultado);	
+		
 			}
 		} );
-		location.href = "portfolio-3-col.html";
+		
 		
 	});
 
